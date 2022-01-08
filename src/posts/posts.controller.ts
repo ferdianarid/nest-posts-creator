c7f2734ffe2d262bfa018fae110c9140d17a80a0
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-09 02:31:13 +0700

    update controller posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,22 +1,27 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreatePostsDto } from './dto/posts-create.dto';
 import { Posts } from './entity/posts.entities';
 import { PostsService } from "./posts.service"
 
+@ApiTags("Posts")
 @Controller('posts')
 export class PostsController {
        constructor(private postsServices: PostsService) { }
 
+       @ApiOkResponse({ type: Posts, isArray: true })
        @Get()
-       getPosts(): Posts[] {
-              return this.postsServices.findAllPosts()
+       getPosts(@Query("title") title: string): Posts[] {
+              return this.postsServices.findAllPosts(title)
        }
 
+       @ApiOkResponse({ type: Posts, description: "Selected Posts" })
        @Get(":id")
        getPostsById(@Param("id") id: string): Posts {
               return this.postsServices.findPostsById(Number(id))
        }
 
+       @ApiCreatedResponse({ type: Posts })
        @Post()
        createPost(@Body() body: CreatePostsDto): Posts {
               return this.postsServices.createPosts(body)
