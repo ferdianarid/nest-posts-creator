39d77b165d8cdbbafa7d560b2b50a8104ff139b8
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-09 13:35:13 +0700

    update  controller posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, NotFoundException, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
+import { ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { CreatePostsDto } from './dto/posts-create.dto';
 import { Posts } from './entity/posts.entities';
 import { PostsService } from "./posts.service"
@@ -10,15 +10,25 @@ export class PostsController {
        constructor(private postsServices: PostsService) { }
 
        @ApiOkResponse({ type: Posts, isArray: true })
+
+       @ApiQuery({ name: 'title', required: false })
        @Get()
-       getPosts(@Query("title") title: string): Posts[] {
+       getPosts(@Query("title") title?: string): Posts[] {
               return this.postsServices.findAllPosts(title)
        }
 
        @ApiOkResponse({ type: Posts, description: "Selected Posts" })
+
+       @ApiNotFoundResponse()
        @Get(":id")
-       getPostsById(@Param("id") id: string): Posts {
-              return this.postsServices.findPostsById(Number(id))
+       getPostsById(@Param("id", ParseIntPipe) id: number): Posts {
+              const SelectedPosts = this.postsServices.findPostsById(id)
+
+              if (!SelectedPosts) {
+                     throw new NotFoundException()
+              }
+
+              return SelectedPosts
        }
 
        @ApiCreatedResponse({ type: Posts })
